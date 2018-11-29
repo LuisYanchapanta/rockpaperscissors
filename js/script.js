1d91a837a82f0773db9e3c4ae1f555da5bb7c7cd
Luis Yanchapanta <yanchapanta2019@gmail.com>
2018-11-29 23:20:21 +0000

    I added a reaction to both choices

--- js/script.js
+++ js/script.js
@@ -8,25 +8,82 @@
 
 
 // DOCUMENT READY FUNCTION BELOW
-  var userChoice = "";
-  var computerChoice = "";
+  
+  
   var winner = "";
-  var randomNumber = 0;
+  
+function makeComputerChoice(){
+    var randomNumber = Math.random();
+    console.log(randomNumber);
+    
+    var computerChoice = "";
+    
+    if(randomNumber < 1/3) {
+        computerChoice = "Rock";
+    } else if(randomNumber < 2/3) {
+        computerChoice = "Paper";
+    } else {
+        computerChoice = "Scissors";
+    }   
+    
+    return computerChoice;
+}
 
 $("#shoot").click(function(){
-    userChoice = $("#input").val();
-    randomNumber = Math.random();
-    $("#userChoice").text(userChoice);
-    console.log(randomNumber)
+    $("#result").text("");
     
-     if(randomNumber < 1/3) {
-        $("#computerChoice").text("Rock"); 
-    } else if(randomNumber < 2/3) {
-        $("#computerChoice").text("Paper"); 
-    } else {
-        $("#computerChoice").text("Scissors"); 
+    var userChoice = $("#input").val();
+    console.log(userChoice);
+    
+    if (userChoice === "") {
+        $("#result").text("Try Again");
+        return;
+    }
+    
+    $("#userChoice").text(userChoice);
+    
+    var computerChoice = makeComputerChoice();
+    console.log(computerChoice);
+    
+    $("#computerChoice").text(computerChoice);
+    
+    if(userChoice === "Rock" && computerChoice == "Rock"){
+        $("#result").text( "Draw!");
+    }
+    
+    if(userChoice === "Rock" && computerChoice == "Paper"){
+        $("#result").text( "Computer has won : (");
+    }
+    
+    if(userChoice === "Rock" && computerChoice == "Scissors"){
+        $("#result").text( "You Win! : )");
+    }
+    
+    if(userChoice == "Paper" && computerChoice == "Paper"){
+        $("#result").text( "Draw!");
+    }
+    
+    if(userChoice == "Paper" && computerChoice == "Rock"){
+        $("#result").text( "You Win! : )");
+    }
+    
+    if(userChoice == "Paper" && computerChoice == "Scissors"){
+      $("#result").text( "Computer has won : (");
+    }
+    
+    if(userChoice == "Scissors" && computerChoice == "Rock"){
+        $("#result").text( "Computer has won : (");
+    }
+    
+    if(userChoice == "Scissors" && computerChoice == "Paper"){
+        $("#result").text( "You Win! : )");
+    }
+    
+    if(userChoice == "Scissors" && computerChoice == "Scissors"){
+        $("#result").text( "Draw!");
     }
 });
+
 
 
 
